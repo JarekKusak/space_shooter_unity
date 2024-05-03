fd1716ca613eabb194d51acf269197d212282e27
Jaroslav Kusák <jarekusak@seznam.cz>
2024-05-03 15:59:56 +0200

    statické zobrazování UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,12 +5,13 @@ using UnityEngine.UI; // Pro použití UI komponent
 public class UIManager : MonoBehaviour
 {
     public TextMeshPro healthText;
-    public TextMeshPro scoreText;
+    public TextMeshProUGUI scoreText;
 
     private int score = 0;
     private int aliensKilled = 0;
 
     public void UpdateHealth(int currentHealth) {
+        Debug.Log("NECO SE DEJE");
         healthText.text = "[" + new string('|', currentHealth) + "]";
     }
 
